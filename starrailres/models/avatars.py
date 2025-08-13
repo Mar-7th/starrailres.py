e855e849c4f72ec15516166f0ecdff51f85c3748
mobyw <mobyw66@gmail.com>
2025-08-13 21:59:24 +0800

    :sparkles: Make avatar icon optional

--- starrailres/models/avatars.py
+++ starrailres/models/avatars.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from msgspec import Struct
 
@@ -6,7 +6,7 @@ from msgspec import Struct
 class AvatarType(Struct):
     id: str  # avartar id
     name: str  # avartar name
-    icon: str  # avartar icon
+    icon: Optional[str]  # avartar icon
 
 
 AvatarIndex = Dict[str, AvatarType]
